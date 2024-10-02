27e46553875d31f79b13a9de98bf17b6b0221aef
EricJDarnell <eric.j.darnell@gmail.com>
2024-10-02 19:27:37 -0400

    fun stuff added

--- index.js
+++ index.js
@@ -1,50 +1,68 @@
 // Complete the Numbers class below
 // the constructor has already been provided
 class Numbers {
-    constructor(data) {
-      //data can either be a string or an array of numbers
-      if (typeof data === "string") {
-        this.data = str.split(",").map((number) => number * 1);
-      } else {
-        this.data = data;
-      }
-    }
-    count() {
-      //return the count of numbers in data
-    }
-    printNumbers() {
-      //print the numbers in data
-    }
-    odds() {
-      //return the odd numbers in data
-    }
-    evens() {
-      //return the even numbers in data
-    }
-    sum() {
-      //return the sum of the numbers
-    }
-    product() {
-      //return the product of the numbers
-    }
-    greaterThan(target) {
-      //return the numbers greater than the target
-    }
-    howMany(target) {
-      //return the count of a given number
+  constructor(data) {
+    //data can either be a string or an array of numbers
+    if (typeof data === "string") {
+      this.data = str.split(",").map((number) => number * 1);
+    } else {
+      this.data = data;
     }
   }
-  
-  //Prompt the user for a list of integers separated by commas
-  const str = prompt("enter some numbers, like this", "1,2,3,3,5,9");
-  
-  //create an instance of numbers
-  const n1 = new Numbers(str);
-  console.log(n1.count()); //returns count of numbers
-  n1.printNumbers(); //prints the number along with their indexes
-  console.log(n1.odds()); //returns odd numbers
-  console.log(n1.evens()); //returns even numbers
-  console.log(n1.sum()); //returns sum of numbers
-  console.log(n1.product()); //returns product of numbers
-  console.log(n1.greaterThan(3)); //returns numbers greater than another number
-  console.log(n1.howMany(3)); //return the count of a specific number
+  count() {
+    //return the count of numbers in data
+    return this.data.length;
+  }
+  printNumbers() {
+    //print the numbers in data
+    this.data.forEach((number) => console.log(number)); // prints individually
+    console.log(this.data); // prints array
+  }
+  odds() {
+    //return the odd numbers in data
+    return this.data.filter((number) => number % 2 !== 0);
+  }
+  evens() {
+    //return the even numbers in data
+    return this.data.filter((number) => number % 2 === 0);
+  }
+  sum() {
+    //return the sum of the numbers
+    return this.data.reduce((accumulator, number) => accumulator + number, 0);
+  }
+  product() {
+    //return the product of the numbers
+    return this.data.reduce((accumulator, number) => accumulator * number, 1);
+  }
+  greaterThan(target) {
+    //return the numbers greater than the target
+    return this.data.filter((number) => number > target);
+  }
+  howMany(target) {
+    //return the count of a given number
+    return this.data.filter((number) => number > target).length;
+  }
+}
+let defaultString = "";
+let randNum1 = Math.floor(Math.random() * 10) + 1; // creates random number 1-10
+let randNum2 = Math.floor(Math.random() * 10); //creates random number 0-9
+
+for (let i = 0; i < randNum1; i++) {
+  defaultString+= (Math.floor(Math.random() * 10)) + ",";
+}
+defaultString+= Math.floor(Math.random() * 10);
+
+//Prompt the user for a list of integers separated by commas
+const str = prompt("enter some numbers, like this", defaultString);
+
+//create an instance of numbers
+const n1 = new Numbers(str);
+console.log(n1.count()); //returns count of numbers
+n1.printNumbers(); //prints the number along with their indexes
+console.log(n1.odds()); //returns odd numbers
+console.log(n1.evens()); //returns even numbers
+console.log(n1.sum()); //returns sum of numbers
+console.log(n1.product()); //returns product of numbers
+console.log("the following values are measured against: " + randNum1)
+console.log(n1.greaterThan(randNum1)); //returns numbers greater than another number
+console.log(n1.howMany(randNum1)); //return the count of a specific number
